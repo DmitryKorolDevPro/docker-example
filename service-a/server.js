1400e8d8301c3ebe46c37ad9a5feedf96ca577ca
Dmitry Korol <dmitry.korol@dev.pro>
2021-01-24 17:35:21 +0200

    docker example. add networking

--- service-a/server.js
+++ service-a/server.js
@@ -1,12 +1,26 @@
 'use strict';
 const express = require('express');
-
+const http = require('http');
 const port = 8080;
 const host = '0.0.0.0';
-
 const app = express();
+
 app.get('/', (req, res) => {
-  res.send('Hello World');
+  http.get('http://service-b:8081/api', (resp) => {
+    let data = [];
+
+    resp.on('data', (chunk) => {
+      data += chunk;
+    });
+
+    resp.on('end', () => {
+      res.send('All titles that could be found:<br />' + JSON.parse(data).map(item => `- ${item.title}<br />`).join(''));
+    });
+
+  }).on("error", (err) => {
+    res.send("Error: " + err.message);
+  });
+
 });
 
 app.listen(port, host);
--- service-b/server.js
+++ service-b/server.js
@@ -5,9 +5,34 @@ const port = 8081;
 const host = '0.0.0.0';
 
 const app = express();
-app.get('/', (req, res) => {
-  res.send('Hello World');
+ 
+const FakeDB = require('fake-db');
+const db = new FakeDB([
+    {title: 'foo'},
+    {title: 'bar'}
+]);
+
+app.get('/api', (req, res) => {
+  db.getCollection().then(function(collection) {
+    res.json(collection);
+  });
+  // res.send('Hello World, service');
 });
 
 app.listen(port, host);
 console.log(`running on http://${host}:${port}`);
+
+
+
+
+// 'use strict';
+
+
+ 
+// app.get('/api/todos', function(req, res) {
+//   db.getCollection().then(function(collection) {
+//     res.json(collection);
+//   });
+// });
+ 
+// app.listen(8081);
