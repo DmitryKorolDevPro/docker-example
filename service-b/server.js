b584c8990136f1e611209d2a3742a01e718e28cc
Dmitry Korol <dmitry.korol@dev.pro>
2021-01-24 17:37:36 +0200

    docker example. remove dead code

--- service-b/server.js
+++ service-b/server.js
@@ -8,31 +8,15 @@ const app = express();
  
 const FakeDB = require('fake-db');
 const db = new FakeDB([
-    {title: 'foo'},
-    {title: 'bar'}
+    { title: 'foo' },
+    { title: 'bar' }
 ]);
 
 app.get('/api', (req, res) => {
   db.getCollection().then(function(collection) {
     res.json(collection);
   });
-  // res.send('Hello World, service');
 });
 
 app.listen(port, host);
 console.log(`running on http://${host}:${port}`);
-
-
-
-
-// 'use strict';
-
-
- 
-// app.get('/api/todos', function(req, res) {
-//   db.getCollection().then(function(collection) {
-//     res.json(collection);
-//   });
-// });
- 
-// app.listen(8081);
